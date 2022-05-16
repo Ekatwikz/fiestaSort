49956f2d6f1fe8261e986c74cfec25c0744067f6
ekatwikz <ekatwikz@gmail.com>
2022-05-16 12:46:03 +0200

    small addition to README, cleaned up fiestaSort.c

--- README.md
+++ README.md
@@ -12,3 +12,7 @@ wild right? xD
 
 I remember some languages were able to do the original question in less than 5 *bytes* but I don't think that's possible in C lmao  
 anyway, I originaly wrote this code a while back, but I wasn't using git/github much at that point, might as well upload it now
+
+also iirc, a number of the answers I saw were [Bogosorts](https://en.wikipedia.org/wiki/Bogosort) of some form, kinda just generating each permutation  
+but this one is provably [O(n<sup>2</sup>)](https://en.wikipedia.org/wiki/Time_complexity) despite having a single mysterious goto  
+xD
--- fiestaSort.c
+++ fiestaSort.c
@@ -1,40 +1,29 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <stdbool.h>
-
-#define USAGE_STRING "min max sz"
+#define USAGE_STRING "min max sz\n"\
+	"min - lower bound for randomizer in test\n"\
+	"max - upper \"\n"\
+	"sz - amount of random numbers"
 #define ERR_MULTIPROCESS 0
 #include "katwikOpsys.h"
 
-void randArray(int* arr, size_t sz, int min, int max) {
-	for (size_t i = 0; i < sz; ++i)
-		arr[i] = min + rand() % (max - min + 1);
-}
-
-void printArray(int* arr, size_t sz) {
-	for (size_t i = 0; i < sz; ++i)
-		printf("%d ", arr[i]);
-	printf("\n");
-}
-
-bool checkArray(int* arr, size_t sz) {
-	for (size_t i = 1; i < sz; ++i)
-		if (arr[i - 1] > arr[i])
-			return false;
-	return true;
-}
-
-// sorting with ZERO if statements, ZERO loops, and ZERO (explicit) swaps?!
-// boutta do some devious shit xD
+// you can ignore this chunk, it doesn't actually compile anything extra,
+// but it asks GCC to shut up just for these functions, because parts of this are
+// just borderline undefined behaviour lol
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Wsequence-point"
 #pragma GCC diagnostic ignored "-Wparentheses"
 #pragma GCC diagnostic ignored "-Wpedantic"
+
+void fiestaSort(int* arr, size_t sz) {
+	size_t i=1,j=1;
+x:
+	j?arr[j--]^=arr[j-1]>arr[j]?arr[j-1]^=arr[j]^=arr[j-1]:0:(j=++i)<sz?:({return;});
+	goto x;
+}
+
 void fiestaSortCLEANISH(int* arr, size_t sz) {
 	size_t i = 1, j = 1;
 
-theWildestTernaryStatementIveEverWrittenLmfao:
+thiccTernary:
 	j ? // if j isnt 0,
 		arr[j--] ^= // decrement j after (just one of many sequence point
 			// abuses here xD) we:
@@ -45,7 +34,7 @@ theWildestTernaryStatementIveEverWrittenLmfao:
 				// (x = x xor y; y = y xor x; x = x xor y;)
 				// == swap(x, y)
 				// notice how in all this, that implies it's still
-				// a stable sort lmfao),
+				// kinda a stable sort lmfao),
 			: 0 // otherwise we don't swap
 				// (NB: x XOR 0 == x, quite cute)
 		: // but if j IS already 0,
@@ -54,20 +43,20 @@ theWildestTernaryStatementIveEverWrittenLmfao:
 				// , we're done
 	;
 
-	goto theWildestTernaryStatementIveEverWrittenLmfao; // otherwise we
+	goto thiccTernary; // otherwise we
 	    // keep going
 }
 
-void fiestaSortGOLFED(int* arr, size_t sz) {
-	size_t i=1,j=1;
-x:
-	j?arr[j--]^=arr[j-1]>arr[j]?arr[j-1]^=arr[j]^=arr[j-1]:0:(j=++i)<sz?:({return;});
-	goto x;
-}
+// and now we're done clowning lol
 #pragma GCC diagnostic pop
 
+// stuff for testing
+void randArray(int* arr, size_t sz, int min, int max);
+void printArray(int* arr, size_t sz);
+bool checkArray(int* arr, size_t sz);
+
 int main(int argc, char** argv) {
-	// setup
+	// get arguments and setup
 	USAGE(argc > 3);
 	int min = atoi(argv[1]);
 	int max = atoi(argv[2]);
@@ -75,17 +64,45 @@ int main(int argc, char** argv) {
 	size_t sz = atoi(argv[3]);
 	USAGE(sz > 1);
 
-	// random numbers
+	// make and print array of random numbers
 	srand(time(NULL));
 	int* arr = malloc(sz * sizeof(int));
 	randArray(arr, sz, min, max);
 	printArray(arr, sz);
 
-	// sexiest sorting algorithm xD
-	fiestaSortGOLFED(arr, sz);
+	// sort the array and print it
+	fiestaSort(arr, sz);
 	printArray(arr, sz);
-	printf(checkArray(arr, sz) ? "Sorted!\n" : "OOPS!\n");
+	
+	// test if it's actually sorted
+	bool arrayWasSorted = checkArray(arr, sz);
+	printf_(arrayWasSorted ? "Sorted!\n" : "OOPS!\n");
 
+	// cleanup and exit
 	FREE(arr);
-	return EXIT_SUCCESS;
+	return arrayWasSorted ? EXIT_SUCCESS : EXIT_FAILURE;
+}
+
+void randArray(int* arr, size_t sz, int min, int max) {
+	for (size_t i = 0; i < sz; ++i) {
+		arr[i] = myRand(min, max);
+	}
+}
+
+void printArray(int* arr, size_t sz) {
+	for (size_t i = 0; i < sz; ++i) {
+		printf_("%d ", arr[i]);
+	}
+
+	printf_("\n");
+}
+
+bool checkArray(int* arr, size_t sz) {
+	for (size_t i = 1; i < sz; ++i) {
+		if (arr[i - 1] > arr[i]) {
+			return false;
+		}
+	}
+
+	return true;
 }
